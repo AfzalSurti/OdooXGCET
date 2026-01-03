cba9627c8d98239991fc1ea1a6018d6e72da885f
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-01-03 10:14:10 +0530

    added userSchema

---
+++ Backend/prisma/migrations/20260103044235/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('EMPLOYEE', 'HR');
+
+-- DropTable
+DROP TABLE "Test";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'EMPLOYEE',
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "emailVerifyToken" TEXT,
+    "emailVerifyExpires" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,14 +3,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/",async (req,res)=>{
-  await prisma.test.create({
-      data : {
-        name : "Ronak Singh"
-      }
-    })
 
-    res.send("OK");
-})
+
 
 app.listen(3000);
