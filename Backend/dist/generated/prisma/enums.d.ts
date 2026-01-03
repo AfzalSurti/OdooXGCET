6fefa65acdd726fc602a15d814521a69a8bc7808
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-01-03 12:29:14 +0530

    Remove dist folder from tracking

--- Backend/dist/generated/prisma/enums.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const Role: {
-    readonly EMPLOYEE: "EMPLOYEE";
-    readonly HR: "HR";
-};
-export type Role = (typeof Role)[keyof typeof Role];
-//# sourceMappingURL=enums.d.ts.map
